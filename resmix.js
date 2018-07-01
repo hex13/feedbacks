8d9c6b2205cccb91a9fbedf2ea851fa9c3be38f8
Łukasz Lityński <hex13code@gmail.com>
2018-07-01 13:34:13 +0200

    simple function/promise based effects

--- resmix.js
+++ resmix.js
@@ -1,18 +1,33 @@
 const MATCH = Symbol();
+const EFFECTS = Symbol();
+
+const UPDATE = '@@resmix/update';
 
 exports.reducerFor = (blueprint) => {
     return (state, action) => {
+        if (action.type == UPDATE) {
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value
+            }
+        }
         let updates = {};
+        let effects = [];
         Object.keys(blueprint).forEach(k => {
             const pairs = blueprint[k].pairs;
 
             pairs.forEach(([pattern, reducer]) => {
                 if (pattern == action.type) {
-                    updates[k] = reducer(state[k], action);
+                    const result = reducer(state[k], action);
+                    if (typeof result == 'function') {
+                        effects.push([k, result]);
+                    } else {
+                        updates[k] = result;
+                    }
                 }
             });
         });
-        return { ...state, ...updates};
+        return { ...state, ...updates, [EFFECTS]: effects};
 
     };
 };
@@ -23,3 +38,19 @@ exports.match = (pairs) => {
         pairs,
     }
 };
+
+exports.middleware = store => next => action => {
+    next(action);
+    const state = store.getState();
+    const effects = state[EFFECTS];
+    if (effects) {
+        effects.forEach(([k, run]) => {
+            Promise.resolve(run()).then(value => {
+                next({type: UPDATE, payload: {
+                    name: k,
+                    value,
+                }});
+            })
+        })
+    }
+}
--- test/reduxInterationSpec.js
+++ test/reduxInterationSpec.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
 const Resmix = require('../resmix');
 
-const { createStore } = require('redux');
-describe('resmix', () => {
+const { createStore, applyMiddleware } = require('redux');
+
+describe('[resmix]', () => {
     it('should allow for declare pattern/reducer pairs', () => {
         const INC = 'inc';
         const DEC = 'dec';
@@ -23,4 +24,38 @@ describe('resmix', () => {
         store.dispatch({type: DEC});
         assert.deepStrictEqual(store.getState(), {counter: 1, counterX2: 2});
     });
+
+    describe('[effects]', () => {
+        let store;
+
+        let promise = Promise.resolve(124);
+        beforeEach(() => {
+            store = createStore(Resmix.reducerFor({
+                future: Resmix.match([
+                    ['fetch', (state, action) => () => promise]
+                ])
+            }), {future: null}, applyMiddleware(Resmix.middleware));
+        });
+
+        it('should init to the correct state', () => {
+            assert.deepStrictEqual(store.getState(), {future: null});
+        });
+
+        it('when reducer returns a function, it should not assign it to the property', () => {
+            store.dispatch({type: 'fetch'});
+            assert.deepStrictEqual(store.getState(), {future: null});
+        });
+
+        it('should allow for returning effect, i.e. function that returns a promise. And value of that promise should be put in the property', 
+            () => {
+                store.dispatch({type: 'fetch'});
+                return promise.then(value => {
+                    assert.deepStrictEqual(store.getState(), {future: 124});
+                });
+        });
+
+    });
+
+
+
 });
