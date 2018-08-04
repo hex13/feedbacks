350905210a3a7d038e269cd226a4b1df6b404790
Łukasz Lityński <hex13code@gmail.com>
2018-08-04 22:44:27 +0200

    update calendar example to use effect api

--- examples/calendar/src/store.js
+++ examples/calendar/src/store.js
@@ -1,7 +1,9 @@
 import * as Redux from 'redux';
-import { withRedux, init, createEngine } from 'feedbacks';
+import { withRedux, init, ac } from 'feedbacks';
 import * as fx from 'feedbacks/fx';
 import { forward, backward, showDetail, addNote } from './actions';
+import * as Rx from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
 const now = new Date;
 
@@ -9,6 +11,7 @@ const getKeyByDate = (params) => {
     return `${params.year}-${params.month}-${params.day}`;
 };
 
+const doLoad = ac('load');
 const blueprint = {
     month: init(1)
         .on(forward({ target: 'month' }), v => v + 1)
@@ -25,7 +28,7 @@ const blueprint = {
         date: init({ day: now.getDate(), month: now.getMonth() + 1, year: now.getFullYear() })
             .on(showDetail(), (_, { payload }) => payload),
         note: init({text: '....'})
-            .on(showDetail(), (_, { payload }) => fx.load(payload))
+            .on(showDetail(), (_, { payload }) => fx.effect(doLoad(payload)))
     },
         
     notesByDay: init({ a: { text: 'something' } })
@@ -39,16 +42,26 @@ const blueprint = {
         })
 }; 
 
+const delay = (t,v) => {
+    return () => new Promise(r => {
+        setTimeout(() => {
+            r(v);
+        }, t)
+    })
+}
+
 export default function configureStore() {
     
-    const engine = createEngine(blueprint);
+    const store = withRedux(Redux).createEngine(blueprint)
+    .onEffect(doLoad(), (dispatch, getState, {payload:date}) => {
+        const note = getState().notesByDay[getKeyByDate(date)] || {text: '???'};
+        
+        return fx.flow([
+            {text: '...loading...'},
+            delay(500, note),
+        ]);
+    })
+    .getStore();
 
-    engine.loader((params, state) => {
-        const note = state.notesByDay[getKeyByDate(params)];
-        return note || {text: '[not found]'};
-    });
-
-    return Redux.createStore(engine.reducer, Redux.applyMiddleware(
-        engine.middleware
-    ));
+    return store;
 }
