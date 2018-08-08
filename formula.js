cbd579459fb4687a97f478b408cffcf1371ac6a3
Łukasz Lityński <hex13code@gmail.com>
2018-08-08 12:25:59 +0200

    move Formula to separate file

---
+++ formula.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const { isMatch } = require('./matching');
+
+class Formula {
+    constructor(recipe) {
+        if (recipe) {
+            Object.assign(this, recipe);
+        }
+    }
+    init(value) {
+        const recipe = new Formula(this);
+        recipe.initialState = value;
+        recipe.hasInitialState = true;
+        return recipe;
+    }
+    // return new Recipe object with given pattern and data
+    on(...args) {
+        return this.match(...args);
+    }
+    match(...args) {
+        const recipe = new Formula(this);
+        recipe.hasMatchPairs = true;
+
+        if (args.length == 2) {
+            recipe.pairs = (recipe.pairs || []).concat([args]);
+        } else {
+            const pairs = args[0];
+            recipe.pairs = pairs;
+        }
+        return recipe;
+    }
+    // executes pattern-matching and runs callback
+    doMatch(action, onMatch) {
+        let matched = false;
+        const pairs = this.pairs;
+        
+        if (pairs) for (let i = 0; i < pairs.length; i++) {
+            let [pattern, reducer] = pairs[i];
+            if (matched)
+                return;
+            let equal = isMatch(pattern, action);
+            if (equal) {
+                onMatch(reducer);
+                matched = true;
+            }
+        } else console.log("####^", this);
+
+        function search(node) {
+            if (node instanceof Formula) {
+                node.doMatch(action, onMatch);
+                return;
+            }
+            if (!isPlainValue(node)) for (let k in node) {
+                search(node[k])
+            }
+        }
+        if (this.hasInitialState) {
+         //   search(this.initialState)
+        }
+    }
+    itemsLike(itemBlueprint, mapActionToKey) {
+        const recipe = new Formula(this);
+        recipe.itemBlueprint = itemBlueprint;
+        recipe.mapActionToKey = mapActionToKey;
+        return recipe;
+    }
+};
+
+module.exports = Formula;
--- resmix.js
+++ resmix.js
@@ -5,7 +5,7 @@ const debug = console.log.bind(console);
 const EFFECTS = Symbol('effects');
 
 const BLUEPRINT = Symbol('blueprint');
-
+const { isMatch } = require('./matching');
 const UPDATE = '@@resmix/update';
 const UPDATE_ROOT = '@@resmix/updateRoot';
 const UPDATE_BLUEPRINT = '@@resmix/updateBlueprint';
@@ -14,8 +14,8 @@ const symbolObservable = require('symbol-observable').default;
 const { get, set } = require('transmutable/get-set');
 const { MUTATION } = require('transmutable/symbols');
 const { applyPatch } = require('transmutable/transform');
-const { isMatch } = require('./matching');
 const EffectRunner = require('./effectRunner');
+const Formula = require('./formula');
 const fx = require('./fx');
 const { createEffect, EFFECT, spawn } = fx;
 const nop = ()=>{};
@@ -223,70 +223,6 @@ const reducerFor = () => {
     };
 };
 
-class Formula {
-    constructor(recipe) {
-        if (recipe) {
-            Object.assign(this, recipe);
-        }
-    }
-    init(value) {
-        const recipe = new Formula(this);
-        recipe.initialState = value;
-        recipe.hasInitialState = true;
-        return recipe;
-    }
-    // return new Recipe object with given pattern and data
-    on(...args) {
-        return this.match(...args);
-    }
-    match(...args) {
-        const recipe = new Formula(this);
-        recipe.hasMatchPairs = true;
-
-        if (args.length == 2) {
-            recipe.pairs = (recipe.pairs || []).concat([args]);
-        } else {
-            const pairs = args[0];
-            recipe.pairs = pairs;
-        }
-        return recipe;
-    }
-    // executes pattern-matching and runs callback
-    doMatch(action, onMatch) {
-        let matched = false;
-        const pairs = this.pairs;
-        
-        if (pairs) for (let i = 0; i < pairs.length; i++) {
-            let [pattern, reducer] = pairs[i];
-            if (matched)
-                return;
-            let equal = isMatch(pattern, action);
-            if (equal) {
-                onMatch(reducer);
-                matched = true;
-            }
-        } else console.log("####^", this);
-
-        function search(node) {
-            if (node instanceof Formula) {
-                node.doMatch(action, onMatch);
-                return;
-            }
-            if (!isPlainValue(node)) for (let k in node) {
-                search(node[k])
-            }
-        }
-        if (this.hasInitialState) {
-         //   search(this.initialState)
-        }
-    }
-    itemsLike(itemBlueprint, mapActionToKey) {
-        const recipe = new Formula(this);
-        recipe.itemBlueprint = itemBlueprint;
-        recipe.mapActionToKey = mapActionToKey;
-        return recipe;
-    }
-};
 
 exports.match = (...args) => {
     return new Formula().match(...args);
